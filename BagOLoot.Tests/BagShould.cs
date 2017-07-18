28e9a7258576049df83c090eb4ad62d47b9e65af
Chaz Henricks <chazhenricks@gmail.com>
2017-07-18 15:58:46 -0500

    started writing tests

---
+++ BagOLoot.Tests/BagShould.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BagOLoot.Tests
+{
+    public class BagShould
+    {
+        private readonly Bag _bag;
+
+        public BagShould()
+        {
+            _bag = new Bag();
+        }
+
+        public void AddItemToBag(string item)
+        {
+            
+        }
+    }
+}
---
+++ BagOLoot/Bag.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.Sqlite;
+
+namespace BagOLoot
+{
+    public class Bag
+    {
+
+    }
+}
